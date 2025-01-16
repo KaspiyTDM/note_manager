11536999709c8748d26e8435aa881c4e1e054002
Artem Ogirya <t1m0x4rus73@gmail.com>
2025-01-16 18:42:05 +0300

    data_changer.py

--- data_changer.py
+++ data_changer.py
@@ -1,9 +1,6 @@
-from datetime import datetime
-created_date = input("Введите дату в формате ДД.ММ.ГГГГ: ")
-issue_date = input("Введите дату в формате ДД.ММ.ГГГГ: ")
-created_data_object = datetime.strptime(created_date, "%d.%m.%Y")
-issue_data_object = datetime.strptime(issue_date, "%d.%m.%Y")
-temp_created_date = created_data_object.strftime("%d %B")
-temp_issue_date = issue_data_object.strftime("%d %B")
-print(f"Дата создания: {temp_created_date}")
-print(f"Дата окончания: {temp_issue_date}")
+created_date = "16-01-2025" 
+issue_date = "29-01-2025"    
+temp_created_date = datetime.strptime(created_date, "%d.%m.%Y").strftime("%d.%m")
+temp_issue_date = datetime.strptime(issue_date, "%d.%m.%Y").strftime("%d.%m")
+print("Дата создания:", temp_created_date)
+print("Дата истечения:", temp_issue_date)
