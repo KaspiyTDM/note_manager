fda738932c5f462c87ade20afcb9b11c357507cb
Artem Ogirya <t1m0x4rus73@gmail.com>
2025-01-21 16:13:25 +0300

    Update add_list.py

--- add_list.py
+++ add_list.py
@@ -1,7 +1,7 @@
 title = input("Заголовок №1: ")
 title1 = input("Заголовок №2: ")
 title2 = input("Заголовок №3: ")
-title_list = ([title.capitalize(),title1.capitalize(),title2.capitalize()])
+title_list = [title.capitalize(),title1.capitalize(),title2.capitalize()]
 print("Заголовок №1: ",title.capitalize())
 print("Заголовок №2: ",title1.capitalize())
 print("Заголовок №3: ",title2.capitalize())
