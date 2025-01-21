f40e7e105895f69036be971bbfef12e282c6798b
Artem Ogirya <t1m0x4rus73@gmail.com>
2025-01-21 15:50:01 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,8 @@
+title = input("Заголовок №1: ")
+title1 = input("Заголовок №2: ")
+title2 = input("Заголовок №3: ")
+title_list = ([title.capitalize(),title1.capitalize(),title2.capitalize()])
+print("Заголовок №1: ",title.capitalize())
+print("Заголовок №2: ",title1.capitalize())
+print("Заголовок №3: ",title2.capitalize())
+print(title_list)
